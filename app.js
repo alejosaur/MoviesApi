22f669360ecb31ee4bfc8a445eaa20b7dd52cc3a
Alejandro Santamaria Perez <alejandro.santamaria@ibm.com>
2020-01-16 09:39:14 -0500

    cors

--- app.js
+++ app.js
@@ -18,6 +18,13 @@ var visualRecognition = new VisualRecognitionV3({
 // Parse incoming requests data
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+  next();
+});
 
 app.get('/api/v1/parameters', (req, res) => {
   res.status(200).send({
